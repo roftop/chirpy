69df2a974b4a351a60dc34f55b29edc36ad151c4
roftop <diogoafvalente@gmail.com>
2026-09-25 10:01:30 +0100

    added png file

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir(".")))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
