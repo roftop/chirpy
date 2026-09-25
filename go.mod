0a4e36dc5d8304de3fd1237db53eb2aa66af8fa8
roftop <diogoafvalente@gmail.com>
2026-09-25 09:54:16 +0100

    created main.go and index.html

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/roftop/chirpy
+
+go 1.26.5
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServe())
+}
